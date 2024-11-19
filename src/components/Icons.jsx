8bb2056801a06112d2754a7fb914c61361c0da4d
Muffakir Hamid <muffakir63@gmail.com>
2024-11-19 06:16:21 +0530

    Half dome

---
+++ src/components/Icons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Icons() {
+  return (
+    <div className='flex p-4 m-4 border-'>
+        <div>
+        <img src="src\assets\asset 63.jpeg" className='h-14' alt="" />
+        <p>Farms</p>
+        </div>
+    </div>
+  )
+}
+
+export default Icons
